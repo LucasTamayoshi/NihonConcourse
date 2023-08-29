92a0668132360f0b091df6c13c7328e485d3edac
LucasTamayoshi <143478668+LucasTamayoshi@users.noreply.github.com>
2023-08-29 14:08:30 +0900

    Add files via upload

---
+++ coprime_square_number.py
@@ -0,0 +1,8 @@
+import math
+def coprime_squares(n):
+    import math
+    coprime_squares_number=[]
+    for a in range(1,int(n**(1/2))+1):
+        if math.gcd(a,n)==1:
+            coprime_squares_number.append(a)
+    return len(coprime_squares_number)
